bdc24e4972a98faffad75afe3927fb94aa64e030
Dylan Cannella <dylancannella@gmail.com>
2024-12-06 16:04:36 -0500

    Update main.py
    I had to change def main to make the UI work, pls double check and make sure I didn't break it on accident.
    
    Also, I need to add the constants right? Was thinking I could pull these straight from tic tac toe (let me know), listed below:
    
    BOARD_ROWS = 3
    BOARD_COLS = 3
    SQUARE_SIZE = 200
    CIRCLE_RADIUS = 60
    CIRCLE_WIDTH = 15
    CROSS_WIDTH = 25
    SPACE = 55
    RED = (255, 0, 0)
    BG_COLOR = (255, 255, 245)
    LINE_COLOR = (245, 152, 66)
    CIRCLE_COLOR = (155, 155, 155)
    CROSS_COLOR = (66, 66, 66)
    WIDTH = 600
    HEIGHT = 600
    LINE_WIDTH = 15
    WIN_LINE_WIDTH = 15
    BOARD_ROWS = 3
    BOARD_COLS = 3
    SQUARE_SIZE = 200
    CIRCLE_RADIUS = 60
    CIRCLE_WIDTH = 15
    CROSS_WIDTH = 25
    SPACE = 55
    RED = (255, 0, 0)
    BG_COLOR = (255, 255, 245)
    LINE_COLOR = (245, 152, 66)
    CIRCLE_COLOR = (155, 155, 155)
    CROSS_COLOR = (66, 66, 66)

--- main.py
+++ main.py
@@ -6,6 +6,138 @@ cell_size = 50  # only change this one to make the cells smaller/bigger
 width = cell_size * 9
 height = cell_size * 9 + cell_size
 
+WIDTH, HEIGHT = width, height
+LINE_COLOR = (0, 0, 0)
+
+# UI functionality is below
+def draw_game_start(screen):  # creates the game start screen
+    # title font
+    start_title_font = pygame.font.Font(None, 100)
+    game_mode_font = pygame.font.Font(None, 75)
+    button_font = pygame.font.Font(None, 50)
+
+    # background color
+    screen.fill((128, 150, 255))
+
+    # title
+    title_surface = start_title_font.render("Sudoku!", 0, (0, 0, 0))
+    title_rectangle = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+    screen.blit(title_surface, title_rectangle)
+
+    # game modes
+    game_mode_surface = game_mode_font.render("Select Game Mode:", 0, (0, 0, 0))
+    game_mode_rectangle = game_mode_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    screen.blit(game_mode_surface, game_mode_rectangle)
+
+    # buttons and text
+    easy_text = button_font.render("Easy", 0, (255, 255, 255))
+    medium_text = button_font.render("Medium", 0, (255, 255, 255))
+    hard_text = button_font.render("Hard", 0, (255, 255, 255))
+
+    # text and button background color
+    easy_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    easy_surface.fill(LINE_COLOR)
+    easy_surface.blit(easy_text, (10, 10))
+    medium_surface = pygame.Surface((medium_text.get_size()[0] + 20, medium_text.get_size()[1] + 20))
+    medium_surface.fill(LINE_COLOR)
+    medium_surface.blit(medium_text, (10, 10))
+    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surface.fill(LINE_COLOR)
+    hard_surface.blit(hard_text, (10, 10))
+
+    # button rectangle
+    easy_rectangle = easy_surface.get_rect(center=(WIDTH // 2 - 200, HEIGHT // 2 + 100))
+    medium_rectangle = medium_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 100))
+    hard_rectangle = hard_surface.get_rect(center=(WIDTH // 2 + 200, HEIGHT // 2 + 100))
+
+    # buttons
+    screen.blit(easy_surface, easy_rectangle)
+    screen.blit(medium_surface, medium_rectangle)
+    screen.blit(hard_surface, hard_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_rectangle.collidepoint(event.pos):
+                    # checks if mouse on easy button
+                    return "easy"  # returns to main if the mouse on start button
+                elif medium_rectangle.collidepoint(event.pos):
+                    # returns to main if the mouse on medium button
+                    return "medium"
+                elif hard_rectangle.collidepoint(event.pos):
+                    return "hard"
+        pygame.display.update()
+
+
+def draw_ingame_options(screen): #creates buttons and text and colors for in game options
+    button_font = pygame.font.Font(None, 50)
+    reset_text = button_font.render("Reset", 0, (255, 255, 255))
+    restart_text = button_font.render("Restart", 0, (255, 255, 255))
+    exit_text = button_font.render("Exit", 0, (255, 255, 255))
+
+    reset_surface = pygame.Surface((reset_text.get_size()[0] + 20, reset_text.get_size()[1] + 20))
+    reset_surface.fill(LINE_COLOR)
+    reset_surface.blit(reset_text, (10, 10))
+    restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
+    restart_surface.fill(LINE_COLOR)
+    restart_surface.blit(restart_text, (10, 10))
+    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
+    exit_surface.fill(LINE_COLOR)
+    exit_surface.blit(exit_text, (10, 10))
+
+    reset_rectangle = reset_surface.get_rect(center=(WIDTH // 2 - 200, HEIGHT - 25))
+    restart_rectangle = restart_surface.get_rect(center=(WIDTH // 2, HEIGHT - 25))
+    exit_rectangle = exit_surface.get_rect(center=(WIDTH // 2 + 200, HEIGHT - 25))
+
+    # buttons
+    screen.blit(reset_surface, reset_rectangle)
+    screen.blit(restart_surface, restart_rectangle)
+    screen.blit(exit_surface, exit_rectangle)
+
+    return reset_rectangle, restart_rectangle, exit_rectangle
+
+def draw_game_over(screen): # creates game over screen, same procedures as previous functions
+    game_over_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 50)
+
+    screen.fill((128, 150, 255))  # background color
+    title_surface = game_over_font.render("Game Lost", 0, (0, 0, 0))
+    title_rectangle = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+    screen.blit(title_surface, title_rectangle)
+
+    restart_text = button_font.render("Restart", 0, (255, 255, 255))
+    restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
+    restart_surface.fill(LINE_COLOR)
+    restart_surface.blit(restart_text, (10, 10))
+
+    restart_rectangle = restart_surface.get_rect(center=(WIDTH // 2, HEIGHT - 50))
+    screen.blit(restart_surface, restart_rectangle)
+
+    pygame.display.flip()
+    return restart_rectangle
+
+def draw_game_win(screen): # creates game win screen, same procedures as previous functions
+    game_win_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 50)
+
+    screen.fill((128, 150, 255))
+
+    title_surface = game_win_font.render("Game Won!", 0, (0, 0, 0))
+    title_rectangle = title_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+    screen.blit(title_surface, title_rectangle)
+
+    exit_text = button_font.render("Exit", 0, (255, 255, 255))
+    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
+    exit_surface.fill(LINE_COLOR)
+    exit_surface.blit(exit_text, (10, 10))
+
+    exit_rectangle = exit_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 100))
+    screen.blit(exit_surface, exit_rectangle)
+
+    pygame.display.flip()
+    return exit_rectangle
 
 def tempGen():
     list = []
@@ -217,80 +349,106 @@ class Board:
                 ]
                 if not is_solved(grid):
                     return False
+                    
+    def is_solved(self): # checks if board is solved
+        return self.check_board()
 
         return True
 
-def main():
+def main(): # NOTE FROM DYLAN: i changed a ton of stuff in main so that the UI would work, lmk if this is wrong/bad and i can undo and figure it out
     try:
         pygame.init()
         screen = pygame.display.set_mode((width, height))
-        board = Board(9, screen, "temp")  # temporary call, should be done through the main menu
-        # cell definition goes here
+        pygame.display.set_caption("Sudoku")
+        
         clock = pygame.time.Clock()
-        currentCell = [0, 0]
         running = True
-        sketchNum = None #used in cell editting
+        currentCell = [0, 0]
+        sketchNum = None # used in cell editting
+        state = "start"
+        board = None
+
         while running:
-            for event in pygame.event.get():
-                match event.type:
-                    case pygame.QUIT:
+            if state == "start":  # displays start screen
+                difficulty = draw_game_start(screen)
+                if difficulty:
+                    board = Board(9, screen, difficulty)
+                    state = "game"
+
+            elif state == "game":
+                reset_rectangle, restart_rectangle, exit_rectangle = draw_ingame_options(screen)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
                         running = False
                     
-                    case pygame.MOUSEBUTTONDOWN: #cell nav by mouse
+                    elif event.type == pygame.MOUSEBUTTONDOWN: # cell nav by mouse
                         clickX, clickY = event.pos
                         selected = board.click(clickX, clickY)
                         if selected:
                             currentCell = [selected[0], selected[1]]
                             board.select(selected[0], selected[1])
 
-                        '''
-                        clickX = list(event.pos)[0]
-                        clickY = list(event.pos)[1]
-                        cellRow = clickX // cell_size
-                        cellCol = clickY // cell_size
-                        currentCell = [cellRow, cellCol]
-                        '''
-                    
-                    # checks all key user inputes for arrow nav and cell writing
-                    case pygame.KEYDOWN:
-                        match event.key:
-                            case pygame.K_RIGHT: #arrow keys for naviation
-                                if currentCell[0] < 8:
-                                    currentCell[0] += 1
-                            case pygame.K_LEFT:
-                                if currentCell[0] > 0:
-                                    currentCell[0] -= 1
-                            case pygame.K_UP:
-                                if currentCell[1] > 0:
-                                    currentCell[1] -= 1
-                            case pygame.K_DOWN:
-                                if currentCell[1] < 8:
-                                    currentCell[1] += 1
+                        # in game button interactions
+                        if reset_rectangle.collidepoint(event.pos):
+                            board.reset_to_original()
+                        elif restart_rectangle.collidepoint(event.pos):
+                            state = "start"
+                        elif exit_rectangle.collidepoint(event.pos):
+                            running = False
 
-                            case pygame.K_RETURN: #enter sketch
-                                print(f"submitted {sketchNum} at {currentCell}") #TODO replace with call to board.sketch
-
-                            case pygame.K_BACKSPACE:
+                    elif event.type == pygame.KEYDOWN: # checks all key user inputes for arrow nav and cell writing
+                        if event.key == pygame.K_RIGHT and currentCell[0] < 8: # arrow keys for navigation
+                            currentCell[0] += 1
+                        elif event.key == pygame.K_LEFT and currentCell[0] > 0:
+                            currentCell[0] -= 1
+                        elif event.key == pygame.K_UP and currentCell[1] > 0:
+                            currentCell[1] -= 1
+                        elif event.key == pygame.K_DOWN and currentCell[1] < 8:
+                            currentCell[1] += 1
+                            
+                        elif event.key == pygame.K_RETURN: # enter sketch
+                            if sketchNum:
+                                board.place_number(sketchNum)
+                            # print(f"submitted {sketchNum} at {currentCell}"), i replaced with call to board
+                        elif event.key == pygame.K_BACKSPACE:
+                            board.clear() # pass, i replaced with call to the clear function/whatever wipes the cells sketch/entered number
+                        else:
+                            try:
+                                sketchNum = int(event.unicode) # checks for number inputs to sketch
+                                if sketchNum == 0:
+                                    sketchNum = None
+                                board.sketch(sketchNum)
+                                # print(f"num pressed: {sketchNum}"), i replaced with call to board.place_number
+                            except ValueError: #not a number
                                 pass
-                                #TODO make call to the clear function/whatever wipes the cells sketch/entered number
 
-                            case _: #checks for number inputs to sketch
-                                try:
-                                    sketchNum = int(event.unicode)
-                                    if sketchNum == 0:
-                                        sketchNum = None
-                                    print(f"num pressed: {sketchNum}") #TODO replace with call to board.place_number
-                                except: #not a number
-                                    continue
-                    case _:
-                        continue                       
-                        continue
+                # checks if the board is full
+                if board.is_full():
+                    if board.is_solved():
+                        state = "win"
+                    else:
+                        state = "end"
 
-            screen.fill("white")
-            board.draw()
-            pygame.display.flip()
+                # creates the game screen
+                screen.fill("white")
+                board.draw()
+                pygame.display.flip()
+
+            elif state == "end":
+                restart_rectangle = draw_game_over(screen)
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if restart_rectangle.collidepoint(event.pos):
+                            state = "start"
+
+            elif state == "win":
+                exit_rectangle = draw_game_win(screen)
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if exit_rectangle.collidepoint(event.pos):
+                            running = False
+
             clock.tick(60)
-        generate_sudoku(9, 0)
     finally:
         pygame.quit()
 
